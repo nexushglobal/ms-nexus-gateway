e6fa73edc4b259ccd18730240851c181e93060e6
JosmenDev <josmendev@gmail.com>
2025-07-31 15:40:35 -0500

    feat(points): added payments details endpoint

--- src/point/controllers/points-transaction.controller.ts
+++ src/point/controllers/points-transaction.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Get, Inject, Query, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Inject,
+  Param,
+  ParseIntPipe,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { UserId } from 'src/common/decorators/current-user.decorator';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/common/guards/roles.guard';
 import { POINT_SERVICE } from 'src/config/services';
@@ -24,6 +33,18 @@ export class PointsTransactionController {
         userId,
         ...findPointsTransactionDto,
       },
+    );
+  }
+
+  @Get('transaction-details/:id')
+  getPointsTransactionDetails(
+    @UserId() userId: string,
+    @Param('id', ParseIntPipe) id: number,
+    @Query() paginationDto: PaginationDto,
+  ) {
+    return this.pointClient.send(
+      { cmd: 'pointsTransaction.getUserPointsTransactionPayments' },
+      { id, userId, paginationDto },
     );
   }
 }
