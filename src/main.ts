12c27603262b24032da5c919497d4c33c24109f1
JosmenDev <josmendev@gmail.com>
2025-08-22 11:24:59 -0500

    feat(monthly-volume): added endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { envs } from './config/envs';
 import { RpcCustomExceptionFilter } from './common/filters/http-exception.filter';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
+import { envs } from './config/envs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
--- src/point/controllers/points-lots-transaction.controller.ts
+++ src/point/controllers/points-lots-transaction.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get, Inject, Query, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { UserId } from 'src/common/decorators/current-user.decorator';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/common/guards/roles.guard';
 import { POINT_SERVICE } from 'src/config/services';
+import { CreateLotPointsDto } from '../dto/create-lot-direct-bonus.dto';
 import { FindPointsTransactionDto } from '../dto/find-weekly-volume.dto';
 
 @Controller('points-lots-transaction')
@@ -12,6 +21,14 @@ export class LotPointsTransactionController {
   constructor(
     @Inject(POINT_SERVICE) private readonly pointClient: ClientProxy,
   ) {}
+
+  @Post('create-points')
+  createLotPoints(@Body() body: CreateLotPointsDto) {
+    return this.pointClient.send(
+      { cmd: 'pointsLotTransaction.createLotPoints' },
+      body,
+    );
+  }
 
   @Get('transaction')
   getTransaction(
